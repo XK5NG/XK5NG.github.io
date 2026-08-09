e201c141c159dab6f9a6e50d5419bc228d5a92b1
XK5NG <166706090+XK5NG@users.noreply.github.com>
2026-08-09 23:01:14 +0800

    Update XK5NG_ui_lib.lua

--- XK5NG_ui_lib.lua
+++ XK5NG_ui_lib.lua
@@ -1,2 +1,3 @@
 --ok vro
+script_key = "%KEY%" -- why would polsec need script key for trial mode now omg
 return loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/6dc5ee831ff32b32bd87f4040b1ec13d007fd25d04c46c787dceee08184a8d51.lua"))()
